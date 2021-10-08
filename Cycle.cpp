37b13a9b618719402031ddfff843275fd0cea285
QuitePixel <92144432+QuitePixel@users.noreply.github.com>
2021-10-08 09:39:59 +0300

    Create Cycle.cpp

---
+++ Cycle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+ using namespace std;
+ int main ()
+ {
+     float x, a, f;
+     for (x=-3; x<=2; x+=0.3)
+     {if (fabs(sin(x))>1/2)
+     f=pow(x+a,2);
+     else
+     f=x+1;
+    cout<<"\n x="<<x<<"\t f="<<f;
+     }
+     
+ }
